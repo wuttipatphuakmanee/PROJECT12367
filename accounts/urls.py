a4045b2d1896a3d4e54a747d57cfeaa88098910b
wuttipatphuakmanee <wuttipatphuakmaneeo@gmail.com>
2025-04-27 22:55:09 +0700

    Add files via upload

---
+++ accounts/urls.py
@@ -0,0 +1,10 @@
+# accounts/urls.py
+
+from django.urls import path
+from .views import register_view, login_view, logout_view
+
+urlpatterns = [
+    path('register/', register_view, name='register'),
+    path('login/', login_view, name='login'),
+    path('logout/', logout_view, name='logout'), # <-- บรรทัดนี้กำหนด name='logout'
+]
---
+++ accounts/views.py
@@ -0,0 +1,30 @@
+
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, logout
+from django.contrib.auth.forms import AuthenticationForm
+from .forms import RegisterForm, LoginForm
+
+def register_view(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('room_list')
+    else:
+        form = RegisterForm()
+    return render(request, 'accounts/register.html', {'form': form})
+
+def login_view(request):
+    if request.method == 'POST':
+        form = LoginForm(data=request.POST)
+        if form.is_valid():
+            login(request, form.get_user())
+            return redirect('room_list')
+    else:
+        form = LoginForm()
+    return render(request, 'accounts/login.html', {'form': form})
+
+def logout_view(request):
+    logout(request)
+    return redirect('login')
